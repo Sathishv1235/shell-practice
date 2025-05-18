4303198b188a6f5c0e5c13a545b72adf3b361e21
Sathishv1235 <sathish.1235@gmail.com>
2025-05-18 18:54:46 +1000

    Execution of Bash Shell

---
+++ 08.Conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "please enter a number"
+read value
+if[ value gt 10 ]
+{
+
+    echo "Given Number $value is greater than 10"
+}
+    else
+{
+    echo "Given Number $value is not greater than 10"
+}
