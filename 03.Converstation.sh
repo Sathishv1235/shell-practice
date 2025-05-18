79f2faa38af55806ed5390646ace6e8ca1474981
Sathishv1235 <sathish.1235@gmail.com>
2025-05-18 16:22:42 +1000

    Execution of Bash Shell

--- 03.Converstation.sh
+++ 03.Converstation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-name1=$1
-name2=$2
+$name1=$1
+$name2=$2
 
-echo "name1:: Hey name2, How are you?"
-echo "name2:: Hello name1, I am fine. How are you doing"
-echo "name1: I am fine too. What's up?"
-echo "name2:: Nohting, just going to Mars now, will you come?"
-echo "name1:: Sorry, you carry on! I will come once you come back"
+echo "$name1:: Hey $name2, How are you?"
+echo "$name2:: Hello $name1, I am fine. How are you doing"
+echo "$name1: I am fine too. What's up?"
+echo "$name2:: Nohting, just going to Mars now, will you come?"
+echo "$name1:: Sorry, you carry on! I will come once you come back"
