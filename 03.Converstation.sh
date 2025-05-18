ded854e05a7e5cd43f474f47b4920cf5de9ec559
Sathishv1235 <sathish.1235@gmail.com>
2025-05-18 17:15:04 +1000

    Execution of Bash Shell

--- 03.Converstation.sh
+++ 03.Converstation.sh
@@ -2,9 +2,11 @@
 
 #1=Trump
 #2=Musk
+name1=$1
+name2=$2
 
-echo "$1:: Hey $2, How are you?"
-echo "$2:: Hello $1, I am fine. How are you doing"
-echo "$1: I am fine too. What's up?"
-echo "$2:: Nohting, just going to Mars now, will you come?"
-echo "$1:: Sorry, you carry on! I will come once you come back"
+echo "$name1:: Hey $name2, How are you?"
+echo "$name2:: Hello $name1, I am fine. How are you doing"
+echo "$name1: I am fine too. What's up?"
+echo "$name2:: Nohting, just going to Mars now, will you come?"
+echo "$name1:: Sorry, you carry on! I will come once you come back"
