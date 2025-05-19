95d450516b7ce1cb35ab2892d322ed463605c25a
Sathishv1235 <sathish.1235@gmail.com>
2025-05-19 20:31:54 +1000

    Shell Scripting

--- 07.SpecialVariables.sh
+++ 07.SpecialVariables.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "All arguments passed here are using '$@': $@"
-echo "No.of Arguments passed $#"
-echo "Script name $0"
-echo "Present Working Directory: $PWD"
-echo "User name who runs is $USER"
-echo "User home directory: $HOME"
-echo "Process ID of script: $$"
+echo "All arguments passed here are using \$@: $@"
+echo "No.of Arguments passed \$#:$#"
+echo "Script name \$0:$0"
+echo "Present Working Directory \$PWD: $PWD"
+echo "User name who runs is \$USER: $USER"
+echo "User home directory \$HOME: $HOME"
+echo "Process ID of script \$$: $$"
 sleep 40 &
-echo "Process ID of previous command: $!" 
+echo "Process ID of previous command \$!: $!" 
